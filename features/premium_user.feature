7d2abb72571daf91769a9042f199f4436d85abd5
JB-tester-uk <149318946+JB-tester-uk@users.noreply.github.com>
2023-11-24 16:13:01 +0000

    update based on review comments, use should as the action word and minor improvement to the given step for the url option.

--- features/premium_user.feature
+++ features/premium_user.feature
@@ -1,19 +1,22 @@
 Feature: Premium user
 
-Scenario: Check premium user response contains premium playlist
-            Given this url "https://a8e38tulbj.execute-api.eu-west-2.amazonaws.com/api/playlists/premium"
-            When the premium user request is sent
+Background: Set base URL
+            Given a base URL "https://a8e38tulbj.execute-api.eu-west-2.amazonaws.com/api/playlists/"
+
+Scenario: Check premium user response should contain premium in the playlist
+            Given this url user option "premium"
+            When the user GET request is sent
             Then we should receive a "200" response
             And "playlists" has field "name" and should contain "Premium"
 
-Scenario: Check free user response DOES NOT contain premium playlist
-            Given this url "https://a8e38tulbj.execute-api.eu-west-2.amazonaws.com/api/playlists/free"
-            When the premium user request is sent
+Scenario: Check free user response SHOULD NOT contain premium in the playlist
+            Given this url user option "free"
+            When the user GET request is sent
             Then we should receive a "200" response
-            And "playlists" has field "name" and DOES NOT contain "Premium"
+            And "playlists" has field "name" and SHOULD NOT contain "Premium"
 
-Scenario: Check other user response contains "Unknown user type"
-            Given this url "https://a8e38tulbj.execute-api.eu-west-2.amazonaws.com/api/playlists/other"
-            When the premium user request is sent
+Scenario: Check other user response should contain "Unknown user type"
+            Given this url user option "other"
+            When the user GET request is sent
             Then we should receive a "400" response
             And response has field "messsage" and should contain "Unknown user type"
--- features/steps/premium_user_steps.py
+++ features/steps/premium_user_steps.py
@@ -1,20 +1,25 @@
 import requests
 from behave import *
 
-@given('this url "{url_string}"')
-def given_a_url_has_been_provide(context, url_string):
-    context.url = str(url_string).strip()
-    assert ((r"http://" in url_string) or
-            (r"https://" in url_string)) == True, f"Error: {url_string} is missing either 'http://' or 'https://'"
+@given('a base URL "{common_url_string}"')
+def given_a_base_url_has_been_provide(context, common_url_string):
+    context.url = str(common_url_string).strip()
 
 
-@when('the premium user request is sent')
+@given('this url user option "{url_option_string}"')
+def given_a_url_user_option_has_been_provide(context, url_option_string):
+    context.url = context.url + str(url_option_string).strip()
+    assert (context.url.startswith(r"http://") or
+            context.url.startswith(r"https://")) == True, f"Error: {url_option_string} does not begin with 'http://' or 'https://'"
+
+
+@when('the user GET request is sent')
 def send_url_get_request(context):
     try:
         context.result = requests.get(context.url)
     except Exception as e:
         print(e)
-        assert False, f"Error: problem with performing a get request"
+        assert False, f"Error: problem with performing a get request, url sent \"{context.url}\""
 
 
 @then('we should receive a "{response_value}" response')
@@ -23,31 +28,28 @@ def check_response_value(context, response_value):
 
 
 @then('"{list_name}" has field "{expected_field}" and should contain "{expected_string}"')
-def check_get_response_contains_expected_string(context, list_name, expected_field, expected_string):
-    json_response = context.result.json()
-    response_name_values = []
-
-    if json_response is not None and list_name in json_response:
-        for element in json_response[list_name]:
-            if expected_field in element:
-                response_name_values.append(element[expected_field])
-    else:
-        assert False, f"Error: get response, list name missing , expected {list_name}"
+def check_get_response_list_should_contain_expected_string(context, list_name, expected_field, expected_string):
+    response_name_values = extract_list_from_json_using_field_name(context, list_name, expected_field)
 
     assert  expected_string in response_name_values, f"Error: get response, expected \"{expected_string}\", actual values {response_name_values} with field name \"{expected_field}\""
 
 
 @then('response has field "{expected_field}" and should contain "{expected_string}"')
-def check_get_response_contains_expected_string(context, expected_field, expected_string):
+def check_get_response_field_should_contain_expected_string(context, expected_field, expected_string):
     json_response = context.result.json()
 
     assert  expected_field in json_response,  f"Error: get response, expected field \"{expected_field}\", actual response \"{json_response}\""
     assert  json_response[expected_field] == expected_string, f"Error: get response, expected \"{expected_string}\", actual \"{json_response[expected_field]}\""
 
 
+@then('"{list_name}" has field "{expected_field}" and SHOULD NOT contain "{expected_string}"')
+def check_get_response_list_should_not_contain_expected_string(context, list_name, expected_field, expected_string):
+    response_name_values = extract_list_from_json_using_field_name(context, list_name, expected_field)
 
-@then('"{list_name}" has field "{expected_field}" and DOES NOT contain "{expected_string}"')
-def check_get_response_contains_expected_string(context, list_name, expected_field, expected_string):
+    assert  expected_string not in response_name_values, f"Error: get response, expected value \"{expected_string}\" SHOULD NOT be present, actual values {response_name_values} with field name \"{expected_field}\""
+
+
+def extract_list_from_json_using_field_name(context, list_name, expected_field):
     json_response = context.result.json()
     response_name_values = []
 
@@ -58,4 +60,4 @@ def check_get_response_contains_expected_string(context, list_name, expected_fie
     else:
         assert False, f"Error: get response, list name missing , expected {list_name}"
 
-    assert  expected_string not in response_name_values, f"Error: get response, expected value \"{expected_string}\" SHOULD NOT be present, actual values {response_name_values} with field name \"{expected_field}\""
+    return response_name_values
